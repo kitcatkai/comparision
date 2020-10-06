0dbc0dc73c8705a8e346d4c618daf82669744355
Kai <tankaiwei@berkeley.edu>
2020-10-06 15:38:29 +0800

    shorten to 2 files

--- R/industry.R
+++ R/industry.R
@@ -14,8 +14,8 @@ NULL
 
 #' @export
 
-load_file <- function(file1, file2, file3, group_by = NULL){
-  df <- read.csv(file1)
+load_file <- function(file1, file3, group_by = NULL){
+  df <- read.csv(file1, na.strings=c("","NA"))
   df[df==""] <- NA
   first_order <- ''
   if (is.null(group_by)) {
@@ -25,12 +25,11 @@ load_file <- function(file1, file2, file3, group_by = NULL){
   }
 
   load_result_1 <- function(first_order) {
-    result <- df %>% tidyr::drop_na() %>%
-      dplyr::group_by(first_order = get(first_order), industry_group_name) %>%
-      dplyr::summarise(total=total) %>%
-      dplyr::mutate(freq = total / sum(total) * 100)
+    result <- df %>%
+      dplyr::group_by(first_order = get(first_order), industry_group_name_a) %>%
+      dplyr::summarise(total = dplyr::n_distinct(member_id))
 
-    result_1 <- ggplot2::ggplot(data=result, ggplot2::aes(x=reorder(industry_group_name, total), y=total, fill=first_order)) + ggplot2::geom_bar(stat = "identity", position = 'dodge') + ggplot2::xlab("Industry") +
+    result_1 <- ggplot2::ggplot(data=result, ggplot2::aes(x=reorder(industry_group_name_a, total), y=total, fill=first_order)) + ggplot2::geom_bar(stat = "identity", position = 'dodge') + ggplot2::xlab("Industry") +
       ggplot2::ylab("Number of Members") + ggplot2::coord_flip() + ggplot2::guides(fill=ggplot2::guide_legend(title=first_order)) + ggplot2::theme_bw()
 
     result_1
@@ -38,31 +37,23 @@ load_file <- function(file1, file2, file3, group_by = NULL){
 
   result_1 <- load_result_1(first_order)
 
-  #Industry Transition Period Plot
-  df <- read.csv(file2,na.strings=c("","NA"))
-  df <- df %>% dplyr::filter(!industry_group_name_b %in% c('Arts', 'Agriculture', 'Public Safety')) %>% dplyr::filter(!industry_group_name_a %in% c('Arts', 'Agriculture', 'Public Safety'))
-  df <- df[complete.cases(df), ]
+  main_duplicate <- df
+  main_duplicate <- main_duplicate %>% dplyr::filter(gap > 0)
 
-  #df$industry_group_name_a <- factor(df$y_industry_group_name_a)
-  #summarise the median and mean
-  #filter out threshold need to fix this hardcode
-
-  result <- df %>%
+  result <- main_duplicate %>% dplyr::filter(gap > 0) %>%
     dplyr::group_by(industry_group_name_b) %>%
-    dplyr::summarise(mean = mean(average_length), median = median(average_length)) %>% dplyr::arrange(desc(median))
+    dplyr::summarise(mean = mean(gap), median = median(gap)) %>% dplyr::arrange(desc(median))
 
-  df$industry_group_name_b <- factor(df$industry_group_name_b, levels = result$industry_group_name_b)
+  main_duplicate$industry_group_name_b <- factor(main_duplicate$industry_group_name_b, levels = result$industry_group_name_b)
 
-  #sort by average
-  result <- df %>%
-    dplyr::group_by(industry_group_name_b) %>%
-    dplyr::summarise(mean = mean(average_length), median = median(average_length))
+  x_axis <- main_duplicate %>%
+    dplyr::summarise(mean = mean(gap), median = median(gap))
 
   #Industry Transition Period Plot
-  result_2 <- ggplot2::ggplot(df , ggplot2::aes(average_length, industry_group_name_b)) + ggplot2::geom_boxplot() +   ggplot2::labs(y="Destination Industry",
-                                                                                    x="Months",
-                                                                                    title = "Time To Transit To An Industry ",
-                                                                                    subtitle="Sorted by Median in Ascending Order") +   #theme with white background
+  result_2 <- ggplot2::ggplot(main_duplicate , ggplot2::aes(gap, industry_group_name_b)) + ggplot2::geom_boxplot() +   ggplot2::labs(y="Destination Industry",
+                                                                                                                                     x="Months",
+                                                                                                                                     title = "Time To Transit To An Industry ",
+                                                                                                                                     subtitle="Sorted by Median in Ascending Order") +   #theme with white background
     ggplot2::theme_bw() +
 
     #eliminates background, gridlines, and chart border
@@ -72,9 +63,7 @@ load_file <- function(file1, file2, file3, group_by = NULL){
       panel.grid.minor = ggplot2::element_blank(),
       panel.border = ggplot2::element_blank()
     ) +
-    ggplot2::geom_vline(ggplot2::aes(xintercept = 3.23), col = "red")
-
-
+    ggplot2::geom_vline(ggplot2::aes(xintercept = unlist(x_axis['mean'])), col = "red")
 
   df <- read.csv(file3)
   result_3 <- ggplot2::ggplot(df, ggplot2::aes(cosine_similarity_group)) +
